4ced55b3a6a3944d42ea0329b4d47a6e5047b495
Sila Setthakan-anan <fresult@Silas-MacBook-Air.local>
2026-02-22 20:26:39 +0700

    feat(dsl): implement basic HTML DSL structure
    - Add , , and  data classes implementing
    - Implement  and  for DSL construction
    - Add  and  helper functions
    - Add  method to test DSL generation
    - Remove old placeholder code

--- src/main/kotlin/com/fResult/practice/KotlinTags.kt
+++ src/main/kotlin/com/fResult/practice/KotlinTags.kt
@@ -12,23 +12,106 @@ object KotlinTags {
    * 3. Define methods that take lambdas with receivers as arguments -> build the DSL
    * 4. test that it works
    */
-
-  // val document = html {
-  //   head {
-  //     title("My Web Page")
-  //   }
-  //   body {
-  //     div(id = "header", className = "main-header") {
-  //       p("Welcome to my web site!")
-  //     }
-  //     div {
-  //       p("This is the start of my site!")
-  //       p("This was rendered with KotlinTags")
-  //     }
-  //   }
-  // }
   sealed interface HtmlElement
-  data class Html(val children: List<HtmlElement>/*...*/) : HtmlElement {
-    override fun toString() = "<html>${children.joinToString("")}</html>"
+
+  // step 1
+  data class Div(
+    val children: List<HtmlElement>,
+    val id: String? = null,
+    val className: String? = null,
+  ) : HtmlElement {
+    override fun toString(): String {
+      val idAttr = id?.let { " id=\"$it\"" } ?: ""
+      val classAttr = className?.let { " class=\"$it\"" } ?: ""
+      val innerHtml = children.joinToString("\n")
+
+      return "<div$idAttr$classAttr>$innerHtml</div>"
+    }
+  }
+
+  data class Body(val children: List<HtmlElement>) : HtmlElement {
+    override fun toString(): String {
+      return children.joinToString("\n", "<body>", "</body>")
+    }
+  }
+
+
+  data class P(val content: String) : HtmlElement {
+    override fun toString(): String {
+      return "<p>$content</p>"
+    }
+  }
+  // TODO: add data types for the rest of the HTML tags
+
+  // step 2
+  class DivBuilder(val id: String?, val className: String?) {
+    private val children = mutableListOf<HtmlElement>()
+
+    fun p(content: String) {
+      children.add(P(content))
+    }
+
+    // expose a "build" method to give us back the final data structure
+    fun build() = Div(children, id, className)
+  }
+
+  class BodyBuilder {
+    private val children = mutableListOf<HtmlElement>()
+    fun div(id: String? = null, className: String? = null, init: DivBuilder.() -> Unit) {
+      children.add(KotlinTags.div(id, className, init))
+    }
+
+    fun p(content: String) {
+      children.add(P(content))
+    }
+
+    fun build() = Body(children)
+  }
+  // TODO: add builders for the rest of the HTML tags: HtmlBuilder, HeadBuilder, and BodyBuilder
+
+  // step 3
+  fun div(id: String? = null, className: String? = null, init: DivBuilder.() -> Unit): Div {
+    val builder = DivBuilder(id, className)
+    builder.init()
+    return builder.build()
+  }
+
+  fun body(init: BodyBuilder.() -> Unit): Body {
+    val builder = BodyBuilder()
+    builder.init()
+    return builder.build()
+  }
+  // TODO: add methods for the DSL for the rest of the HTML tags (move some code around, because, `div` will be a part of BodyBuilder)
+
+  // step 4
+  @JvmStatic
+  fun main(args: Array<String>) {
+
+    val bodyExample = body {
+      div("header", "main-header") {
+        p("Welcome to my web site!")
+      }
+      div {
+        p("This is the start of my site!")
+        p("This was rendered with KotlinTags")
+      }
+    }
+    println(bodyExample)
+    // TODO: only expose the top-level DSL - just the html{} method needs to stay top level
+
+    // val document = html {
+    //   head {
+    //     title("My Web Page")
+    //   }
+    //   body {
+    //     div(id = "header", className = "main-header") {
+    //       p("Welcome to my web site!")
+    //     }
+    //     div {
+    //       p("This is the start of my site!")
+    //       p("This was rendered with KotlinTags")
+    //     }
+    //   }
+    // }
   }
 }
