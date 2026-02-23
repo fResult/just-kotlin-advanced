6326319874fa7948fdb4b188626772283b1397b8
fResult <styxmaz@gmail.com>
2026-02-23 21:12:16 +0700

    feat: add Covariant Vet class to demonstrate declaration-site variance
    - Implement `Vet<out A : Pet>` class
    - Add examples showing `List<Vet<Dog>>` is compatible with `List<Vet<Pet>>`
    - Add comments explaining COVARIANT types in Kotlin

--- src/main/kotlin/com/fResult/typesystem/Variance.kt
+++ src/main/kotlin/com/fResult/typesystem/Variance.kt
@@ -5,6 +5,9 @@ object Variance {
   data class Dog(val name: String) : Pet()
   data class Cat(val name: String) : Pet()
 
+  // COVARIANT generic types
+  data class Vet<out A : Pet>(val pet: A) // <out A> -> COVARIANT IN A
+
   @JvmStatic
   fun main(args: Array<String>) {
     // Dog extends Pet -> List<Dog> "extends" List<Pet>?
@@ -16,5 +19,11 @@ object Variance {
     val fluffy = Cat("Fluffy")
     val pets: List<Pet> = listOf(buddy, max, whiskers, fluffy)
     println(pets)
+
+    // COVARIANT generic types
+    val vetOfDog = Vet(max)
+    val vetOfCat = Vet(whiskers)
+    val vetOfPets: List<Vet<Pet>> = listOf(vetOfCat, vetOfDog)
+    println("Vet of Pets: $vetOfPets")
   }
 }
