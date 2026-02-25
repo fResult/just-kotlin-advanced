409fdec71d0b43ad2264385fbb2ee6986f93afc2
fResult <styxmaz@gmail.com>
2026-02-26 00:36:48 +0700

    docs(typesystem): explain variance restrictions on vars and parameters
    - Document why `var` properties cannot be covariant (`out`) using `Liquid`analogy
    - Document why method parameters cannot be covariant (`out`) using `LList` analogy
    - Clarify input vs output positions for these constructs

--- src/main/kotlin/com/fResult/typesystem/VariancePositions.kt
+++ src/main/kotlin/com/fResult/typesystem/VariancePositions.kt
@@ -25,7 +25,31 @@ object VariancePositions {
    * - types of properties (val or var) are in the "out" (aka covariant) position
    * - "in" types cannot be placed in the "out" position
    */
+
   // class MutableContainer<out A>(var contents: A) // var properties are ALSO in contravariant ("in") position
+  /*
+   * class Liquid
+   * class Water : Liquid()
+   * class Gasoline : Liquid()
+   *
+   * val container: MutableContainer<Liquid> = MutableContainer<Water>(Wallet())
+   * container.contents = Gasoline() // guarantee that I can write any Liquid inside, but have to keep it to Water
+   *
+   * types of vars are in the contravariant ("in") position as well
+   * => Therefore it must be INVARIANT
+   */
+
+  // class LList<out A> {
+  //   // illegal here
+  //   fun add(elem: A): LList<A> = TODO()
+  // }
+  /*
+   * val myList: LList<Animal> = LList<Dog>()
+   * val newList = myList.add(Crocodile()) // guarantee to be able to add any Animal, BUT have to guarantee just Dog
+   *
+   * method parameter types are in the contravariant ("in") position
+   * => Therefore, we cannot use covariant types in method parameters
+   */
 
   @JvmStatic
   fun main(args: Array<String>) {
