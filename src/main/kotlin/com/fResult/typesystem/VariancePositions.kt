f4b1097de737ff5b39ce3c05af7453a2df7e3b22
fResult <styxmaz@gmail.com>
2026-02-27 00:27:06 +0700

    feat(typesystem): solve variance position problems and implement LLList
    - Add examples solving variance conflicts:
      - `LList`: Solve consuming elements in covariant types via type widening
      - `RepairShop`: Solve returning elements in contravariant types via type narrowing
    - Implement `LLList` class to demonstrate practical usage of `@UnsafeVariance` for `add` method
    - Add functional operations (`map`, `filter`, `reduce`) to `LLList`
    - Update `main` to demonstrate these patterns

--- src/main/kotlin/com/fResult/typesystem/VariancePositions.kt
+++ src/main/kotlin/com/fResult/typesystem/VariancePositions.kt
@@ -11,7 +11,6 @@ object VariancePositions {
   // class Vet<in A>(val favoriteAnimal: A) {
   //   fun treat(animal: A) = true
   // }
-
   /*
    * assume that this was legal
    * class Vet<in A>(val favoriteAnimal: A)
@@ -51,8 +50,110 @@ object VariancePositions {
    * => Therefore, we cannot use covariant types in method parameters
    */
 
+  /*
+   * assume this compiled:
+   * class Vet<in A> {
+   *   fun rescueAnimal(): A = TODO()
+   * }
+   *
+   * val vet: Vet<Animal> = object : Vet<Animal> {
+   *   override fun rescueAnimal(): Animal = Cat()
+   * }
+   * val dogVet: Vet<Dog> = vet // legal, because of contravariant
+   * val rescuedDog = dogVet.rescueAnimal() // guarantee to return a Dog, BUT return a Cat
+   *
+   * method return types are in COVARIANT ("out") position
+   */
+
+  /*
+   * solve variance positions problems
+   */
+  // 1 - consume elements in a covariant type
+  abstract class LList<out A>
+  data object EmptyList : LList<Nothing>()
+  data class Cons<out A>(val head: A, val tail: LList<A>) : LList<A>()
+
+  // how do we add an element?
+  // solution = widening type type
+  fun <B, A : B> LList<A>.add(elem: B): LList<B> = Cons(elem, this)
+
+  // 2 - return element in a contravariant type
+  // solution = narrow the type
+  abstract class Vehicle
+  open class Car : Vehicle()
+  class SuperCar : Car()
+
+  class RepairShop<in A : Vehicle> {
+    fun <B : A> repair(elem: B): B = elem
+  }
+
   @JvmStatic
   fun main(args: Array<String>) {
+    // covariant problem
+    val myList: LList<Dog> = EmptyList
+    val dogs = myList.add(Dog()).add(Dog()) // LList<Dog>
+    val animals = dogs.add(Cat()) // LList<Animal> <- Lowest common ancestor
+    println(animals)
 
+    // contravariant problem
+    val repairShop: RepairShop<Car> = RepairShop<Vehicle>() // contravariance
+    val myBeatUpVW = Car()
+    val damagedFerrari = SuperCar()
+
+    val freshVW = repairShop.repair(myBeatUpVW) // Car
+    val freshFerrari = repairShop.repair(damagedFerrari) // SuperCar
+    println(listOf(freshVW, freshFerrari))
+
+    // Mimic Kotlin Native List
+    val xs: LLList<Animal> = LLList.of(Cat(), Dog())
+    val ys: LLList<Animal> = xs.add(Dog())
+    ys.forEach(::println)
+    println(ys.map { it::class.simpleName })
   }
+}
+
+class LLList<out A>(private var items: MutableList<A>) {
+  fun head(): A = TODO()
+  fun tail(): LLList<A> = TODO()
+
+  companion object {
+    fun <A> of(vararg elements: A): LLList<A> {
+      val list = LLList<A>(mutableListOf())
+      for (element in elements) {
+        list.add(element)
+      }
+      return list
+    }
+  }
+
+  // To use a covariant type in a contravariant "in" position (method parameter),
+  // we must introduce a new type parameter B that is a supertype of A.
+  fun add(element: @UnsafeVariance A): LLList<A> {
+    items.add(element)
+    return LLList(items)
+  }
+
+  fun forEach(f: (A) -> Unit) {
+    items.forEach(f)
+  }
+
+  fun <B> map(f: (A) -> B): LLList<B> {
+    val mappedItems = items.map(f)
+    return LLList(mappedItems.toMutableList())
+  }
+
+  fun filter(f: (A) -> Boolean): LLList<A> {
+    val filteredItems = items.filter(f)
+    return LLList(filteredItems.toMutableList())
+  }
+
+  fun <B> reduce(initial: B, reducer: (B, A) -> B): B {
+    var acc = initial
+    for (item in items) {
+      acc = reducer(acc, item)
+    }
+    return acc
+  }
+
+  override fun toString(): String = items.toString()
 }
