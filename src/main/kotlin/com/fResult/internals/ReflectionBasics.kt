9263422840147b2d246f5f715b47bd7214f293c5
fResult <styxmaz@gmail.com>
2026-03-06 22:37:43 +0700

    feat(internals): introduce reflection basics
    - Create `ReflectionBasics.kt` to demonstrate runtime inspection
    - Define `Person` data class as a subject for reflection
    - Implement `main` to show how to access class metadata (names, modifiers, visibility)
    - Demonstrate iterating over declared member properties

---
+++ src/main/kotlin/com/fResult/internals/ReflectionBasics.kt
@@ -0,0 +1,56 @@
+package com.fResult.internals
+
+import kotlin.reflect.KClass
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.jvm.jvmName
+
+object ReflectionBasics {
+  // reflection = inspect and invoke functionality dynamically at runtime
+  data class Person(val name: String, val age: Int) {
+    var favoriteMovie: String? = "Forrest Gump"
+    var phoneNumbers = emptyList<String>()
+
+    fun fillInTaxForm(authority: String) =
+      "[$name] Death and taxes... Filing my tax form for $authority"
+  }
+
+  /*
+   * UI
+   *   select method:
+   *     - fillInTaxForm
+   *     - downloadPersonalData
+   *     ... (parse as a String)
+   */
+  val personClass: KClass<Person> = Person::class
+
+  @JvmStatic
+  fun main(args: Array<String>) {
+    // class reference => class name, methods, properties, ...
+    println("------------- Class basic info -------------")
+    println("Class name: ${personClass.simpleName}")
+    println("Qualified name: ${personClass.qualifiedName}")
+    println("JVM name: ${personClass.jvmName}")
+
+    // class types
+    println("Class is final?: ${personClass.isFinal}")
+    println("Class is abstract?: ${personClass.isAbstract}")
+    println("Class is sealed?: ${personClass.isSealed}")
+    println("Class is open?: ${personClass.isOpen}")
+    println("Class is data?: ${personClass.isData}")
+    println("Class is inner?: ${personClass.isInner}")
+    println("Class is companion?: ${personClass.isCompanion}")
+    println("Class is instance of Any ${personClass.isInstance(Any::class)}")
+
+    // visibility
+    println("access modifier: ${personClass.visibility}")
+
+    // inspect properties at runtime
+    val classProperties = personClass.declaredMemberProperties
+    println("------------- Class properties -------------")
+    classProperties.forEach { prop ->
+      println(
+        "Name: ${prop.name}, Type: ${prop.returnType.classifier}, Is Nullable: ${prop.returnType.isMarkedNullable}"
+      )
+    }
+  }
+}
