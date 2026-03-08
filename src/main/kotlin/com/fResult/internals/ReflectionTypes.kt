c668bf4c6962e93903d789dc38402d6a4bf07f9a
fResult <styxmaz@gmail.com>
2026-03-08 16:50:55 +0700

    feat(internals): implement reflection-based config loader
    - Add `MyConfig` data class to represent configuration
    - Implement `ConfigLoader` to parse key-value paris from a file
    - Add `deserializeObject` inline function to instantiate objects dynamically using constructor reflection (`callBy`)
    - Add `deserializeValue` to handle type conversion from String to primitive types
    - Update `main` to demonstrate loading and deserializing configuration

--- src/main/kotlin/com/fResult/internals/ReflectionTypes.kt
+++ src/main/kotlin/com/fResult/internals/ReflectionTypes.kt
@@ -1,5 +1,7 @@
 package com.fResult.internals
 
+import java.io.File
+import kotlin.reflect.KParameter
 import kotlin.reflect.KType
 import kotlin.reflect.KTypeParameter
 import kotlin.reflect.full.isSubtypeOf
@@ -52,14 +54,80 @@ object ReflectionTypes {
     }
   }
 
+  // more complex example
+  // parsing config files
+  /*
+   * example conf file
+   * host = localhost
+   * port = 8800
+   * debug = true
+   * maxConnections = 100
+   * timeout = 3.2
+   */
+  data class MyConfig(
+    val host: String,
+    val port: Int,
+    val debug: Boolean,
+    val maxConnections: Int,
+    val timeout: Double,
+  )
+
+  // Config.load(...).convertTo<Config>
+
   @JvmStatic
   fun main(args: Array<String>) {
-    val myList: List<*> = listOf(1, 2, 3, 4, 5)
-    processList(listOf(1, 2, 3), typeOf<List<Int>>())
-    processList(listOf("One", "Two", "Three"))
-    processList(myList, typeOf<List<Int>>())
-    processList(myList)
+//    val myList: List<*> = listOf(1, 2, 3, 4, 5)
+//    processList(listOf(1, 2, 3), typeOf<List<Int>>())
+//    processList(listOf("One", "Two", "Three"))
+//    processList(myList, typeOf<List<Int>>())
+//    processList(myList)
 
-    processListV2(listOf("One", "Two", "Three"))
+//    processListV2(listOf("One", "Two", "Three"))
+
+    val loader = ConfigLoader()
+    val props = loader.parseFile()
+    println("Parsed file: $props")
+    val config = loader.deserializeObject<MyConfig>(props)
+    println("Deserialized config: $config")
+    val timeout = config.timeout // now, it's type-safe
+  }
+}
+
+class ConfigLoader(val path: String = "src/main/resources/application.conf") {
+  fun parseFile(): Map<String, String> {
+    val file = File(path)
+    val configMap = mutableMapOf<String, String>()
+    file.forEachLine { line ->
+      val trimmedLine = line.trim()
+      if (trimmedLine.isNotEmpty() && !trimmedLine.startsWith("#")) {
+        val (key, value) = trimmedLine.split("=").map(String::trim)
+        configMap[key] = value
+      }
+    }
+    return configMap
+  }
+
+  fun deserializeValue(value: String, type: KType): Any = when (type.classifier) { // KType.classifier -> KClassifier (supertype of KClass)
+    String::class -> value
+    Int::class -> value.toInt()
+    Double::class -> value.toDouble()
+    Boolean::class -> value.toBoolean()
+    else -> throw IllegalArgumentException("Unsupported type: $type")
+  }
+
+  inline fun <reified T> deserializeObject(props: Map<String, String>): T {
+    // KClass<T> to be able to build an instance of T
+    val kClass = T::class
+    val constructor = kClass.constructors.firstOrNull()
+      ?: throw IllegalArgumentException("Type ${kClass.simpleName} doesn't have an accessible primary constructork")
+    val args: Map<KParameter, Any> = constructor.parameters.associateWith { param ->
+      val key = param.name
+        ?: throw IllegalArgumentException("Unname constructor param for ${kClass.simpleName}")
+      val value = props[key]
+        ?: throw IllegalArgumentException("Missing value for the constructor param ${param.name} in class ${kClass.simpleName}")
+      deserializeValue(value, param.type)
+    }
+
+    return constructor.callBy(args)
   }
 }
