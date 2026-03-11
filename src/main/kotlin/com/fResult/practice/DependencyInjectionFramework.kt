280a4031819bf70668ee07aa37ad121c34c72f33
fResult <styxmaz@gmail.com>
2026-03-11 23:17:33 +0700

    feat(practice): implement recursive constructor injection - Rename `@Layer` annotation to `@Injectable` - Update `@Inject` target to include value parameters - Implement `DependencyInjectionManager#createInstance` to handle recursive constructor resolution - Add `initialize` method to auto-discover and register components - Refactor `main` to demonstrate DI container in action

--- src/main/kotlin/com/fResult/practice/DependencyInjectionFramework.kt
+++ src/main/kotlin/com/fResult/practice/DependencyInjectionFramework.kt
@@ -3,16 +3,19 @@ package com.fResult.practice
 import kotlin.reflect.KClass
 import kotlin.reflect.full.createInstance
 import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.primaryConstructor
 
 object DependencyInjectionFramework {
+  const val ADMIN_USER = "admin@fResult.com"
+
   // part 1 - create 2 annotations
   // @Layer - runtime annotation, can be attached to classes and interfaces ...
   // @Inject - runtime annotation, can be attached to PROPERTIES only
   @Target(AnnotationTarget.CLASS)
   @Retention(AnnotationRetention.RUNTIME)
-  annotation class Layer
+  annotation class Injectable
 
-  @Target(AnnotationTarget.PROPERTY)
+  @Target(AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER)
   @Retention(AnnotationRetention.RUNTIME)
   annotation class Inject
 
@@ -20,58 +23,132 @@ object DependencyInjectionFramework {
   // service - business logic
   // repository - persistence layer
 
-  @Layer
+  @Injectable
   class Repository {
     fun getData(): String = "data from repository"
   }
 
-  @Layer
+  @Injectable
   class Service(@Inject val repository: Repository) {
     fun performAction() = repository.getData() + " - with some business logic"
   }
 
-  @Layer
+  @Injectable
   class Controller(@Inject val service: Service, @Inject val users: UserManager) {
-    fun processHttpRequest(payload: String, username: String = "invalid@fResult.com") =
-      if (users.isLoggedIn(username)) "Processed request: Response ${service.performAction()}"
+    fun processHttpRequest(payload: String, username: String = "invalid@fResult.com"): String {
+      println("Process http request for $payload")
+      return if (users.isLoggedIn(username)) "Processed request: Response ${service.performAction()}"
       else "Not logged in, request denied"
+    }
   }
 
-  @Layer
+  @Injectable
   class UserManager {
-    private val loggedUser = mutableSetOf<String>()
+    private val loggedUsers = mutableSetOf<String>()
 
     fun login(username: String) {
-      loggedUser.add(username)
+      loggedUsers.add(username)
       println("[log] Logged in as $username")
     }
 
-    fun isLoggedIn(username: String) = username in loggedUser
+    fun isLoggedIn(username: String): Boolean {
+      return username in loggedUsers
+    }
 
     fun logout(username: String) {
-      loggedUser.remove(username)
+      loggedUsers.remove(username)
       println("[log] $username just logged out")
     }
   }
 
-  @Inject
-  lateinit var controller: Controller
-
   @JvmStatic
   fun main(args: Array<String>) {
-    val data = controller.processHttpRequest(p)
-    println("Should retrieve data: $data")
+    val diManager = DependencyInjectionManager()
+    diManager.initialize()
+
+    // Get the controller instance
+    val controller = diManager.getInstance(Controller::class)
+
+    // Use it
+    val userManager = diManager.getInstance(UserManager::class)
+    userManager.login(ADMIN_USER)
+
+    val data =
+      controller.processHttpRequest("{ \"source\": \"sensors/incremental\" }", ADMIN_USER)
+    println("Data after login: $data")
+
+    userManager.logout(ADMIN_USER)
+    val moreData =
+      controller.processHttpRequest("{ \"source\": \"sensors/incremental\" }", ADMIN_USER)
+    println("Data after logout: $moreData")
   }
 
   class DependencyInjectionManager {
     private val layers = mutableMapOf<KClass<*>, Any>()
 
-    // part 2.1 - add a function which registers a class into the layers map
-    fun <T : Any> register(clazz: KClass<T>): Unit {
-      val layerAnnotation = clazz.findAnnotation<Layer>()
+    fun initialize() =
+      DependencyInjectionFramework::class.nestedClasses
+        .forEach { register(it) }
+
+    private fun <T : Any> register(clazz: KClass<T>) {
+      val layerAnnotation = clazz.findAnnotation<Injectable>()
       layerAnnotation?.also {
-        layers[clazz] = clazz.createInstance()
+        val instance = createInstance(clazz)
+        if (!layers.containsKey(clazz)) {
+          layers[clazz] = instance
+        }
       }
     }
+
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T : Any> getInstance(clazz: KClass<T>): T {
+      return layers[clazz] as? T
+        ?: throw IllegalArgumentException("Class ${clazz.simpleName} not registered")
+    }
+
+    private fun <T : Any> createInstance(clazz: KClass<T>): T {
+      val constructor = clazz.primaryConstructor ?: return clazz.createInstance()
+
+      val args = constructor.parameters.map { param ->
+        val type = param.type.classifier as KClass<*>
+        // Check if we already have an instance of this type
+        return@map layers[type] ?: createInstance(type).also {
+          // Cache it if created recursively
+          layers[type] = it
+        }
+      }
+
+      return constructor.call(*args.toTypedArray())
+    }
+
+//    private fun <T : Any> injectDependencies(instance: T) {
+//      val clazz = instance::class
+//      clazz.declaredMemberProperties.forEach { prop ->
+//        prop.findAnnotation<Inject>()?.also {
+//          val type = prop.returnType.classifier as KClass<*>
+//          val dependency = layers[type]
+//          if (dependency != null && prop is KMutableProperty<*>) {
+//            prop.setter.call(instance, dependency)
+//          }
+//        }
+//      }
+
+//      val constructor = clazz.primaryConstructor
+////      clazz.members.forEach { member ->
+////        val injectAnnotation = member.findAnnotation<Inject>()
+////      }
+//      val injectedProperties = constructor?.parameters
+//        ?.filter { prop -> prop.findAnnotation<Inject>() != null }
+//        ?: emptyList()
+//      injectedProperties.forEach { prop ->
+//        val type = prop.type.classifier as KClass<*>
+//        val dependency = layers[type]
+//        dependency?.also {
+//          if (it is KMutableProperty<*>)
+//            it.setter.call(instance, createInstance(type))
+//        }
+//      }
+//    }
   }
 }
